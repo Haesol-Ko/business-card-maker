3cd8653c2de1015622d075f00fc2405c80524cbb
Haesol <haesol0828@douzone.com>
2022-08-16 17:13:36 +0900

    init and add auth service

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 import './app.css';
+import Login from "./components/login/login";
 
-function App() {
+function App({authService}) {
+    return <Login authService={authService}/>
 }
 
 export default App;
